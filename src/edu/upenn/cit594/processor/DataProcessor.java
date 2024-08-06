b2ab8408a83a4b2a8c9239b7e3f7457750bd2623
amalsorathia <148710183+amalsorathia@users.noreply.github.com>
2024-08-05 20:38:40 -0700

    Update DataProcessor.java

--- src/edu/upenn/cit594/processor/DataProcessor.java
+++ src/edu/upenn/cit594/processor/DataProcessor.java
@@ -2,4 +2,34 @@ package edu.upenn.cit594.processor;
 
 public class DataProcessor {
 
+    public int getTotalPopulation(ArrayList populationData) {
+		HashMap<Integer, Integer> sumOfPopulation = new HashMap<Integer, Integer>();
+		
+		int totalSum = 0;
+	
+		return getTotalSum();
+		
+	}
+	
+	private int getTotalSum(int totalSum, HashMap<Integer, Integer> sumOfPopulation) {
+		
+		
+		
+		
+		
+		return zipCode;
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+
+
+  
+  
 }
