139a6be3db52db8938680f112167d07902a31e9f
amalsorathia <148710183+amalsorathia@users.noreply.github.com>
2024-08-06 22:54:34 -0700

    Update CovidData.java

--- src/edu/upenn/cit594/util/CovidData.java
+++ src/edu/upenn/cit594/util/CovidData.java
@@ -2,13 +2,13 @@ package edu.upenn.cit594.util;
 
 public class CovidData {
 
-		int zipCode;
+		String zipCode;
 		int partiallyVaccinated;
 		int fullyVaccinated;
 		String dateTime;
 		
 
-		public CovidData(int zipCode, int partiallyVaccinated, int fullyVaccinated, String dateTime) {
+		public CovidData(String zipCode, int partiallyVaccinated, int fullyVaccinated, String dateTime) {
 			this.zipCode = zipCode;
 			this.partiallyVaccinated = partiallyVaccinated;
 			this.fullyVaccinated = fullyVaccinated;
@@ -19,4 +19,25 @@ public class CovidData {
 			return "zipCode " + zipCode + " " + "partiallyVaccinated " + partiallyVaccinated + " " + "fullyVaccinated " + fullyVaccinated + " " + "dateTime " + dateTime;
 			
 		}
+
+		public String getDateTime() {
+			// TODO Auto-generated method stub
+			return this.dateTime;
+		}
+		public String getZipCode() {
+			// TODO Auto-generated method stub
+			return this.zipCode;
+		}
+
+		public Integer getPartialVaccinations() {
+			// TODO Auto-generated method stub
+			return this.partiallyVaccinated;
+		}
+
+		public Integer getFullVaccinations() {
+			// TODO Auto-generated method stub
+			return this.fullyVaccinated;
+		}
+
+
 }
